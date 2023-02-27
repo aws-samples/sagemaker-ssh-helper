240d0ec3013d238f4890061938a4ea098e545ab8
Ivan Khvostishkov <87804596+ivan-khvostishkov@users.noreply.github.com>
2023-02-27 08:49:07 +0100

    Deregister instances: new option --delete-older-than-n-days + moving to SSMManager

--- sagemaker_ssh_helper/deregister_old_instances_from_ssm.py
+++ sagemaker_ssh_helper/deregister_old_instances_from_ssm.py
@@ -1,81 +1,16 @@
 from __future__ import annotations
 
+import logging
+import sys
+
 import boto3
-ssm = boto3.client('ssm')
 
+from sagemaker_ssh_helper.manager import SSMManager
 
-def get_ssm_managed_instances():
-    print('Getting SSM managed instances using pagination')
-    instances = []
-    next_token = ""  # nosec hardcoded_password_string  # not a password
-    while True:
-        response = ssm.describe_instance_information(
-            Filters=[{'Key': 'ResourceType', 'Values': ['ManagedInstance']}],
-            NextToken=next_token,
-            MaxResults=50,
-            )
-        next_token = response.get('NextToken')
-        if response['InstanceInformationList']:
-            instances.extend(response['InstanceInformationList'])
-            print('Appended {} instances'.format(len(response['InstanceInformationList'])))
-        if next_token is None:
-            break
-
-    return instances
-
-
-def filter_instances_regex(instances: list[dict], key, value):
-    import re
-    len_before = len(instances)
-    filtered = []
-    for instance in instances:
-        if key not in instance:
-            print(f"Warning: {key} doesn't exist in {str(instance)}")
-            continue
-        if re.match(value, instance[key], re.IGNORECASE):
-            filtered.append(instance)
-    print(f'Filtered out {len_before-len(filtered)} instances missing a regex match for re.match("{value}","{key}")')
-    return filtered
-
-
-def filter_by_tag(instances: list[dict]):
-    tagname = 'SSHOwner'
-    len_before = len(instances)
-    print(f'Will filter through {len_before} instances, verifying they are tagged with "{tagname}".')
-    filtered = []
-    i = 0
-    for instance in instances:
-        i += 1
-        tags = ssm.list_tags_for_resource(ResourceType='ManagedInstance', ResourceId=instance['InstanceId'])
-        if 'TagList' in tags:
-            for tag in tags['TagList']:
-                if tag['Key'] == tagname:
-                    filtered.append(instance)
-                    break
-        if i % 10 == 0:
-            print(f'In progress... Filtered {i} instances for tag name "{tagname}"')
-    print(f'Filtered out {len_before-len(filtered)} instances missing the tag "{tagname}"')
-    return filtered
-
-
-def filter_to_ssh_helper_instances(prefilter_instances):
-    print('Filtering to SageMaker SSH Helper related instances only')
-    filters_def = [
-        {'key': 'PingStatus', 'value': 'ConnectionLost'},
-    ]
-
-    instances = prefilter_instances
-    for filter_def in filters_def:
-        instances = filter_instances_regex(instances, filter_def['key'], filter_def['value'])
-
-    # TODO: clean up SageMaker Studio instances (can stay down for a while when user switches instances)
-    instances = filter_by_tag(instances)
-
-    return instances
+logging.basicConfig(level=logging.INFO)
 
 
 def is_approved_to_deregister(instance_count):
-    import sys
     if '--preapproving-deregistration' in sys.argv:
         return True
 
@@ -85,9 +20,9 @@ def is_approved_to_deregister(instance_count):
 
 
 def deregister(ssh_helper_instances):
+    ssm = boto3.client('ssm')
     deregistered_success_count = 0
-    for instance in ssh_helper_instances:
-        instance_id = instance['InstanceId']
+    for instance_id in ssh_helper_instances:
         response = ssm.deregister_managed_instance(InstanceId=instance_id)
         if response['ResponseMetadata']['HTTPStatusCode'] == 200:
             print(f'{deregistered_success_count}: Deregistered SSM instance {instance_id}')
@@ -103,16 +38,21 @@ def deregister(ssh_helper_instances):
 def main():
     print('This utility will deregister from SSM all SageMaker SSH Helper related managed instances.')
     print('WARNING: you should be careful NOT deregister managed instances that are not related to SageMaker SSH Helper.')
-    print('Usage: python deregister_old_instances_from_ssm.py [--preapproving-deregistration]')
+    print('Usage: python deregister_old_instances_from_ssm.py [--preapproving-deregistration] [--delete-older-than-n-days <N>]')
     print('--preapproving-deregistration: will automatically approve the deregistration of all instances found, without prompting.')
+    print('--delete-older-than-n-days <N>: will only delete offline instances that are older than N days.')
     print('')
 
-    all_managed_instances = get_ssm_managed_instances()
+    try:
+        index = sys.argv.index('--delete-older-than-n-days')
+        days = int(sys.argv[index + 1])
+    except ValueError or IndexError:
+        days = 0
 
-    print(f'Found {len(all_managed_instances)} managed instances in total in SSM')
-    ssh_helper_instances = filter_to_ssh_helper_instances(all_managed_instances)
+    manager = SSMManager()
+    ssh_helper_instances = manager.list_expired_ssh_instances(days)
+
     num_of_instances_to_deregister = len(ssh_helper_instances)
-    print(f'Found {num_of_instances_to_deregister} managed instances related to SSH Helper')
 
     if is_approved_to_deregister(num_of_instances_to_deregister):
         deregister(ssh_helper_instances)
--- tests/test_deregister_instances.py
+++ tests/test_deregister_instances.py
@@ -1,16 +1,22 @@
 import logging
+import os
 import sys
+
+from mock import mock
 
 from sagemaker_ssh_helper.deregister_old_instances_from_ssm import main as deregister_instances_main
 
 logger = logging.getLogger('sagemaker-ssh-helper')
 
 
+@mock.patch.object(sys, 'argv', ['--preapproving-deregistration', '--delete-older-than-n-days', 2])
 def test_deregister_instances():
-    sys.argv.append('--preapproving-deregistration')
-    try:
-        deregister_instances_main()
-    finally:
-        sys.argv.remove('--preapproving-deregistration')
+    deregister_instances_main()
+    assert True  # nothing to check
 
+
+@mock.patch.dict(os.environ, {"AWS_REGION": "eu-west-2", "AWS_DEFAULT_REGION": "eu-west-2"})
+@mock.patch.object(sys, 'argv', ['--preapproving-deregistration', '--delete-older-than-n-days', 2])
+def test_deregister_instances_another_region():
+    deregister_instances_main()
     assert True  # nothing to check
