b4eb468f74b94d5d9e83a3a7679da5dbca32beb0
Ivan Khvostishkov <87804596+ivan-khvostishkov@users.noreply.github.com>
2023-06-06 16:28:07 +0200

    Renamed local test

--- tests/test_local.py
+++ tests/test_local.py
@@ -1,6 +1,9 @@
 import logging
 import os
 import distutils.dir_util
+from pathlib import Path
+
+import mock
 
 import test_util
 
@@ -17,15 +20,18 @@ def test_local_training():
     Compare with https://github.com/aws/amazon-sagemaker-examples/tree/main/advanced_functionality/scikit_bring_your_own/container/local_test .
     """
     assert training_environment.SAGEMAKER_BASE_PATH == "/opt/ml"
+    assert training_environment.base_dir.endswith("/opt_ml")
 
     test_util._clean_training_opt_ml_dir()
     distutils.dir_util.copy_tree("./source_dir/training_clean/", "./opt_ml/code/")
 
     os.environ = {
-        training_parameters.USER_PROGRAM_ENV: "train_clean.py",
+        training_parameters.USER_PROGRAM_ENV: Path('source_dir/training_clean/train_clean.py').name,
     }
 
     try:
+        # Note: it will start the subprocess, so we don't have the code coverage
+        # TODO: https://coverage.readthedocs.io/en/latest/subprocess.html
         train_main()
     except SystemExit as e:
         assert e.code == 0
@@ -34,3 +40,10 @@ def test_local_training():
     assert os.path.exists("./opt_ml/output/success")
 
     logging.info("Finished training")
+
+
+@mock.patch.dict(os.environ, {"SM_MODEL_DIR": "./opt_ml/model"})
+def test_local_training_with_coverage():
+    # import will start training
+    from source_dir.training_clean import train_clean
+    assert train_clean
