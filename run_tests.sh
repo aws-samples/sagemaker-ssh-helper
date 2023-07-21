2eda5ae4d801ae7aff474cfd0e2a77b6fadebcb1
Ivan Khvostishkov <87804596+ivan-khvostishkov@users.noreply.github.com>
2023-07-21 08:22:15 +0200

    The flag to skip CDK

--- run_tests.sh
+++ run_tests.sh
@@ -27,39 +27,44 @@ apt-get update
 apt-get install -y sudo
 sm-local-configure
 source tests/generate_sagemaker_config.sh
-# Update IAM/SSM policies
-curl -fsSL https://deb.nodesource.com/setup_lts.x | bash -
-export DEBIAN_FRONTEND=noninteractive
-apt-get install -y nodejs
-npm install -g aws-cdk
-cdk --version
-REGION=eu-west-1
-# See https://docs.aws.amazon.com/cdk/v2/guide/bootstrapping.html
-cdk bootstrap aws://"$ACCOUNT_ID"/"$REGION" \
-  --require-approval never
-APP="python -m sagemaker_ssh_helper.cdk.tests_app"
-AWS_REGION=$REGION cdk -a "$APP" deploy SSH-IAM-SSM-Stack-Tests \
-    -c sagemaker_role="$SAGEMAKER_ROLE" -c user_role="$USER_ROLE" \
-    --require-approval never
-APP="python -m sagemaker_ssh_helper.cdk.iam_ssm_app"
-AWS_REGION=$REGION cdk -a "$APP" deploy SSH-IAM-SSM-Stack \
-    -c sagemaker_role="$SAGEMAKER_ROLE" -c user_role="$USER_ROLE" \
-    --require-approval never
-APP="python -m sagemaker_ssh_helper.cdk.advanced_tier_app"
-AWS_REGION=$REGION cdk -a "$APP" deploy SSM-Advanced-Tier-Stack \
-    --require-approval never
-APP="python -m sagemaker_ssh_helper.cdk.low_gpu_lambda_app"
-AWS_REGION=$REGION cdk -a "$APP" deploy Low-GPU-Lambda-Stack \
-    -c sns_notification_topic_arn="$SNS_NOTIFICATION_TOPIC_ARN" \
-    --require-approval never
-REGION=eu-west-2
-cdk bootstrap aws://"$ACCOUNT_ID"/"$REGION" \
-  --require-approval never
-APP="python -m sagemaker_ssh_helper.cdk.advanced_tier_app"
-AWS_REGION=$REGION cdk -a "$APP" deploy SSM-Advanced-Tier-Stack \
-    --require-approval never
-unset REGION
-unset APP
+
+if [ "$SKIP_CDK" == "true" ]; then
+    echo "Skipping CDK changes"
+else
+    echo "Applying CDK changes"
+    curl -fsSL https://deb.nodesource.com/setup_lts.x | bash -
+    export DEBIAN_FRONTEND=noninteractive
+    apt-get install -y nodejs
+    npm install -g aws-cdk
+    cdk --version
+    REGION=eu-west-1
+    # See https://docs.aws.amazon.com/cdk/v2/guide/bootstrapping.html
+    cdk bootstrap aws://"$ACCOUNT_ID"/"$REGION" \
+      --require-approval never
+    APP="python -m sagemaker_ssh_helper.cdk.tests_app"
+    AWS_REGION=$REGION cdk -a "$APP" deploy SSH-IAM-SSM-Stack-Tests \
+        -c sagemaker_role="$SAGEMAKER_ROLE" -c user_role="$USER_ROLE" \
+        --require-approval never
+    APP="python -m sagemaker_ssh_helper.cdk.iam_ssm_app"
+    AWS_REGION=$REGION cdk -a "$APP" deploy SSH-IAM-SSM-Stack \
+        -c sagemaker_role="$SAGEMAKER_ROLE" -c user_role="$USER_ROLE" \
+        --require-approval never
+    APP="python -m sagemaker_ssh_helper.cdk.advanced_tier_app"
+    AWS_REGION=$REGION cdk -a "$APP" deploy SSM-Advanced-Tier-Stack \
+        --require-approval never
+    APP="python -m sagemaker_ssh_helper.cdk.low_gpu_lambda_app"
+    AWS_REGION=$REGION cdk -a "$APP" deploy Low-GPU-Lambda-Stack \
+        -c sns_notification_topic_arn="$SNS_NOTIFICATION_TOPIC_ARN" \
+        --require-approval never
+    REGION=eu-west-2
+    cdk bootstrap aws://"$ACCOUNT_ID"/"$REGION" \
+      --require-approval never
+    APP="python -m sagemaker_ssh_helper.cdk.advanced_tier_app"
+    AWS_REGION=$REGION cdk -a "$APP" deploy SSM-Advanced-Tier-Stack \
+        --require-approval never
+    unset REGION
+fi
+
 # Set bucket versioning to detect model repacking / dependencies overrides
 aws s3api put-bucket-versioning \
     --bucket "$(AWS_DEFAULT_REGION=eu-west-1 bash tests/get_sagemaker_bucket.sh)" \
