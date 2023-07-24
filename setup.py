e0dd7b9102b556e94c07a5777dde8d4b5e505294
Ivan Khvostishkov <87804596+ivan-khvostishkov@users.noreply.github.com>
2023-07-24 08:15:46 +0200

    Adding Python 3.12

--- setup.py
+++ setup.py
@@ -89,5 +89,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ]
 )
