2493e963b534803ea2679d638e673c2a19aef459
Ivan Khvostishkov <87804596+ivan-khvostishkov@users.noreply.github.com>
2023-07-25 10:23:55 +0200

    Version bump to 2.0.1.dev0

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ extras = {
 }
 setuptools.setup(
     name='sagemaker-ssh-helper',
-    version='2.0.0',
+    version='2.0.1.dev0',
     author="Amazon Web Services",
     description="A helper library to connect into Amazon SageMaker with AWS Systems Manager and SSH (Secure Shell)",
     long_description="SageMaker SSH Helper is a library that helps you to securely connect to Amazon SageMaker's "
