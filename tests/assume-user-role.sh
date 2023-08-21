2108575eab372a356eabac59148e6652ce38f094
Ivan Khvostishkov <87804596+ivan-khvostishkov@users.noreply.github.com>
2023-08-21 17:52:44 +0200

    Increasing test runtime to 10 hours

--- tests/assume-user-role.sh
+++ tests/assume-user-role.sh
@@ -1,6 +1,6 @@
 aws sts assume-role \
   --role-arn "$USER_ROLE" \
   --role-session-name "gitlab-ci" \
-  --duration-seconds 28800 \
+  --duration-seconds 36000 \
   --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' \
   --output text
