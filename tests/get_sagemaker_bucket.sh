bd0764944ac8c0abf57fff7f0c44b2d425265801
Ivan Khvostishkov <87804596+ivan-khvostishkov@users.noreply.github.com>
2024-11-27 15:49:12 +0100

    Preparing release v2.2.1

--- tests/get_sagemaker_bucket.sh
+++ tests/get_sagemaker_bucket.sh
@@ -3,6 +3,7 @@
 read -r -d '' program << EOF
 import logging
 logging.getLogger('sagemaker.config').setLevel(logging.WARNING)
+logging.getLogger('botocore.credentials').setLevel(logging.WARNING)
 import sagemaker
 print(sagemaker.Session().default_bucket())
 EOF
